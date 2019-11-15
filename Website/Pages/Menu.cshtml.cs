d18ca44c87e85a3f5b7dfebbcd5fa7f52307237e
Brock Kaufmann <brockk@ksu.edu>
2019-11-14 20:39:31 -0600

    Done

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        /// <summary>
+        /// The menu being used by the form to display items
+        /// </summary>
+        public DinoDiner.Menu.Menu Menu { get; } = new DinoDiner.Menu.Menu();
+         
+        public void OnGet()
+        {
+
+        }
+    }
+}
